b9b95476fc1a508228607aa476531bb54424003c
Maria Sheila Carneiro <msheilacarneiro@gmail.com>
2021-01-07 19:56:43 -0300

    sistema admin

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
+import Home from './Home';
+import './index.css';
+import {BrowserRouter as Router, Route,Switch,Link} from 'react-router-dom';
+
+ReactDOM.render((
+        <Router>
+            <App>
+                    <Switch>            
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/autor" component={AutorBox}/>
+                        <Route path="/livro" component={LivroBox}/>                
+                    </Switch>            
+            </App>
+        </Router>
+
+), document.getElementById('root'));
